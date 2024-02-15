cfd793eb96299544a03a17261a72c834d5446969
shomokh123 <ip5115@outlook.sa>
2024-02-15 11:56:12 +0300

    event controller

--- Sources/App/Controllers/EventController.swift
+++ Sources/App/Controllers/EventController.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import Vapor
+
+struct EventController : RouteCollection {
+    func boot(routes: Vapor.RoutesBuilder) throws {
+        let events = routes.grouped("events")
+        events.get(use: index)
+        events.post(use : create)
+        events.put(":id" , use : update)
+        events.delete(":id" , use : delete)
+        
+    }
+    
+    func index (req : Request) async throws -> String{
+        return "Get all events"
+    }
+    func create (req : Request) async throws -> String{
+        return "Create all plants"
+    }
+    
+    func update (req : Request) async throws -> String{
+        let id = req.parameters.get("id")!
+        return "Update all plants with \(id)"
+    }
+    func delete (req : Request) async throws -> String{
+        let id = req.parameters.get("id")!
+        return "Delete all plants with \(id)"
+    }
+    
+}
