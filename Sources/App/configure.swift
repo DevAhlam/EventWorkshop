af346ec2345cd19cce57642d812190b1e1913544
shomokh123 <ip5115@outlook.sa>
2024-02-18 11:54:01 +0300

    model and migration

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,11 +1,14 @@
 import Vapor
+import Fluent
+import FluentPostgresDriver
 
 // configures your application
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
+    app.databases.use(.postgres(configuration: SQLPostgresConfiguration(hostname: "localhost",  username: "postgres", password:"", database: "eventdb" ,tls: .prefer(try .init(configuration: .clientDefault)))), as: .psql)
     // register routes
+    
     try app.register(collection: EventController())
     try app.register(collection: Eventprovider())
     try routes(app)
