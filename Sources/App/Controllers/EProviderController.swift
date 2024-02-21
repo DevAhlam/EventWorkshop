514ef3b585f5b7d462cceb86105030adb0601aa8
DevAhlam <Ahlamalmutiri.dev@gmail.com>
2024-02-21 10:20:51 +0300

    edite EP

--- Sources/App/Controllers/EProviderController.swift
+++ Sources/App/Controllers/EProviderController.swift
@@ -10,7 +10,7 @@ import Vapor
 
 
 
-struct Eventprovider : RouteCollection {
+struct EventproviderController : RouteCollection {
     
     func boot(routes: Vapor.RoutesBuilder) throws {
         
@@ -18,42 +18,56 @@ struct Eventprovider : RouteCollection {
         let Eventprovider = routes.grouped("Eventprovider")
         
       
-        
-        Eventprovider.get(use : showEvent)
-        
-        
-        Eventprovider.post(use : CreateNewEvent)
-        
-        
-        Eventprovider.put(use: updateEventID)
+        Eventprovider.get(use : showEvent) // all name and email
+        Eventprovider.post(use : CreateNewEvent) // create new Eventprovider
       
-        
-        
-        
-        func showEvent (req : Request) async throws  -> String {
+
+        Eventprovider.group(":id") { EProvider in
+            EProvider.get(use: show) // show event by id
+            EProvider.delete(use: delete) // delete Eventprovider
             
-            return "Show all Event"
+        }}
+        
+       func showEvent(req: Request) async throws -> [EventProvider] {
+                try await EventProvider.query(on: req.db).all()
+            }
+        
+        func CreateNewEvent(req: Request) async throws -> EventProvider {
+                let EventProvider = try req.content.decode(EventProvider.self)
+                try await EventProvider.save(on: req.db)
+                return EventProvider
+            }
+        
+        func show(req: Request) async throws -> EventProvider {
+            guard let EventProvider = try await EventProvider.find(req.parameters.get("id"), on: req.db) else {
+                throw Abort(.notFound)
+            }
+            return EventProvider
+         }
+
+      func delete(req: Request) async throws -> HTTPStatus {
+            guard let EventProvider = try await EventProvider.find(req.parameters.get("id"), on: req.db) else {
+                throw Abort(.notFound)
+            }
+            try await EventProvider.delete(on: req.db)
+            return .ok
         }
+    }
+    
+    
+    
         
         
-        func CreateNewEvent (req : Request) async throws  -> String {
-            
-            return "create New Event"
-        }
         
-        func updateEventID (req : Request) async throws  -> String {
-           
-            return "update Event"
-        }
        
         
      
         
         
-    }
+   
 
     
     
     
-}
+
 
