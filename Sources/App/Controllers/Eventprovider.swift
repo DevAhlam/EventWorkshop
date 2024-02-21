4c53f9c05f2ac35426dac9d4c0fba27155eba67a
DevAhlam <Ahlamalmutiri.dev@gmail.com>
2024-02-21 10:36:59 +0300

    edit

--- Sources/App/Controllers/Eventprovider.swift
+++ Sources/App/Controllers/Eventprovider.swift
@@ -10,7 +10,7 @@ import Vapor
 
 
 
-struct EventproviderController : RouteCollection {
+struct Eventprovider : RouteCollection {
     
     func boot(routes: Vapor.RoutesBuilder) throws {
         
